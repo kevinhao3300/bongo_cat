2ca191ac38bebc24b9a9e604224e1afc0dfe5b61
Kevin Hao <kevinhao3300@yahoo.com>
2022-11-09 01:03:07 -0500

    easiest clap

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var totalCount = 0;
 var background = new Background(["#39FF14", "#4169e1", "#FF69B4"]);
 var cat = new Cat(380, 100, "up.png", "clear_cat.png");
 var table = new Table();
+var timeout = 0;
 
 document.addEventListener("click", clickHandler, false);
 document.addEventListener("keydown", keyDownHandler, false);
@@ -12,13 +13,14 @@ document.addEventListener("keydown", keyDownHandler, false);
 function clickHandler(e) {
   cat.handleClick();
   background.handleClick();
-  ++totalCount;
+  if (timeout === 0) {
+    ++totalCount;
+    timeout = 40;
+  }
 }
 
 function keyDownHandler(e) {
-  cat.handleClick();
-  background.handleClick();
-  ++totalCount;
+  clickHandler(e);
 }
 
 function drawCount() {
@@ -28,6 +30,9 @@ function drawCount() {
 }
 
 function draw() {
+  if (timeout > 0) {
+    --timeout;
+  }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   background.draw(ctx);
   cat.draw(ctx);
